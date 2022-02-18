718ac91cf5d0284c063fd396208ed02a4bce9cdb
rashedsabbir <rashedulhasan1996@gmail.com>
2022-02-18 22:34:53 +0600

    script added

---
+++ js/script.js
@@ -0,0 +1,150 @@
+// clear the input fields values
+
+function clearInputField(elementId) {
+    document.getElementById(elementId).value = '';
+}
+
+
+// reset all fields 
+
+function resetFields() {
+    clearInputField('income-input');
+    clearInputField('food-input');
+    clearInputField('rent-input');
+    clearInputField('cloths-input');
+    clearInputField('save-input');
+
+
+    document.getElementById('total-expenses').innerText = "";
+
+    document.getElementById('balance').innerText = "";
+
+    document.getElementById('saving-amount').innerText = "";
+
+    document.getElementById('remaining-Balence').innerText = "";
+}
+
+
+// Balance and expense calculation 
+
+document.getElementById('calculate-button').addEventListener('click', function () {
+    console.log('button clicked');
+    const income = document.getElementById('income-input').value;
+    const food = document.getElementById('food-input').value;
+    const rent = document.getElementById('rent-input').value;
+    const cloths = document.getElementById('cloths-input').value;
+
+    const con1 = isValidInput(income, 'income-input');
+    const con2 = isValidInput(food, 'food-input');
+    const con3 = isValidInput(rent, 'rent-input');
+    const con4 = isValidInput(cloths, 'cloths-input');
+
+    if (con1 && con2 && con3 && con4) {
+        const expenses = parseFloat(food) + parseFloat(rent) + parseFloat(cloths);
+
+        const errorField = document.getElementById('debit-credit-error');
+        if (expenses > parseFloat(income)) {
+            errorField.innerText = "* Your expense cannot exceed your total income."
+        }
+        else {
+            errorField.innerText = "";
+            document.getElementById('total-expenses').innerText = expenses;
+            const balance = parseFloat(income) - expenses;
+
+            document.getElementById('balance').innerText = balance;
+        }
+    }
+})
+
+
+
+// Savings and remainingBalance calculation 
+
+document.getElementById('save-button').addEventListener('click', function () {
+    let savePercentage = document.getElementById('save-input').value;
+
+    if (isValidInput(savePercentage, 'save-input'));
+
+    savePercentage = parseFloat(savePercentage);
+
+    if (saveValidation(savePercentage)) {
+        const income = document.getElementById('income-input').value;
+
+        const balance = parseFloat(document.getElementById('balance-input').innerText);
+
+        const savingAmount = (income * savePercentage) / 100;
+
+        document.getElementById('saving-amount').innerText = savingAmount;
+
+        const remainingBalance = balance - savingAmount;
+
+        document.getElementById('remaining-Balance').innerText = remainingBalance;
+    }
+})
+
+// Validations involved in the calculation of saving amount
+
+function saveValidation(savePercentage) {
+    const errorField = document.getElementById('save-error');
+    const income = document.getElementById('income-input').value;
+    const balance = document.getElementById('balance-input').innerText;
+
+    let con1 = isValidInput(income, 'income-input');
+    let con2 = false;
+    if (!isNaN(balance) && balance.length > 0) {
+        console.log('Hree');
+        con2 = true;
+    }
+
+    if (con1 && con2) {
+        const savingAmount = (parseFloat(income) * savePercentage) / 100;
+        if (savingAmount > parseFloat(balance)) {
+            errorField.innerText = "* Insufficient fund. Savings cannot exceed balance.";
+            return false;
+        }
+        else {
+            errorField.innerText = "";
+            return true;
+        }
+    }
+    else {
+        errorField.innerText = "* Income or balance is not set properly";
+        return false;
+    }
+}
+
+// Checks validity of the user inputs
+function isValidInput(input, element) {
+    const errorField = document.getElementById(element + '-error');
+    // not a number
+    if (isNaN(input)) {
+        errorField.innerText = "* Input value needs to be a number";
+        return false;
+    }
+    else {
+
+        // empty input field
+        if (input.length == 0) {
+            errorField.innerText = "* Input field cannot be empty";
+            return false;
+        }
+
+
+        const value = parseFloat(input);
+        // negative value
+        if (value < 0) {
+            errorField.innerText = "* Input value needs to be a positive number";
+            return false;
+        }
+        else {
+            // percentage value out of range
+            if (element == 'save' && value > 100) {
+                errorField.innerText = "* Percentage value needs to be inbetween 0 and 100";
+                return false;
+            }
+
+            errorField.innerText = "";
+            return true;
+        }
+    }
+}
